4d3da1a2b04dfdc6d6ae23b712b0e618a7fb2166
bedelman073 <edelman.b@northeastern.edu>
2024-02-21 12:48:58 -0500

    I added a mark as complete function

--- todo_list.py
+++ todo_list.py
@@ -10,11 +10,19 @@ def main():
     if ans==1:
         addtasks(todo_list)
         print("Your to do list has now been updated")
+    if ans==2:
+        markcomplete(todo_list)
 
 main()
 
 def addtasks(todo_list):
     num_tasks=int(input("Enter the amount of tasks you want to add to the list: "))
-    for i in range(num_tasks+1):
+    for i in range(num_tasks):
         task=input("Enter the title of the task: ")
         todo_list[task]="incomplete"
+
+def markcomplete(todo_list):
+    num_tasks=int(input("Enter the amount of tasks you want to mark as complete: "))
+    for i in range(num_tasks):
+        target=input("Enter the name of the task you want to mark as complete: ")
+        todo_list[target]="complete"

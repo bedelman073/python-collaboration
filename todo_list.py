2b7f3ea1f11097dc376708ad7bc81bd9dcfa2d26
bedelman073 <edelman.b@northeastern.edu>
2024-02-21 13:09:26 -0500

    Finishing the program

--- todo_list.py
+++ todo_list.py
@@ -1,26 +1,3 @@
-def main():
-    print("""Welcome to the to do list editior
-    Please enter the number of the command you want
-    1.) Add a new task
-    2.) Mark a task as complete
-    3.) View your list
-    4.) Delete tasks""")
-    ans=input()
-    todo_list={}
-    if ans==1:
-        addtasks(todo_list)
-        print("Your to do list has now been updated")
-    if ans==2:
-        markcomplete(todo_list)
-        print("Your to do list has now been updated")
-    if ans==3:
-        print(todo_list)
-    if ans==4:
-        deltasks(todo_list)
-        print("Your to do list has now been updated")
-
-main()
-
 def addtasks(todo_list):
     num_tasks=int(input("Enter the amount of tasks you want to add to the list: "))
     for i in range(num_tasks):
@@ -38,3 +15,32 @@ def deltasks(todo_list):
     for i in range(num_tasks):
         target=input("Enter the name of the task you want to delete: ")
         del todo_list[target]
+
+def main():
+    run_prog=True
+    todo_list={}
+    while run_prog==True:
+        print("""Welcome to the to do list editior
+    Please enter the number of the command you want
+    1.) Add a new task
+    2.) Mark a task as complete
+    3.) View your list
+    4.) Delete tasks""")
+        ans=int(input("Enter your response here: "))
+        if ans==1:
+            addtasks(todo_list)
+            print("Your to do list has now been updated")
+        if ans==2:
+            markcomplete(todo_list)
+            print("Your to do list has now been updated")
+        if ans==3:
+            print(todo_list)
+        if ans==4:
+            deltasks(todo_list)
+            print("Your to do list has now been updated")
+        cont=int(input("Do you want to access another function? Enter 0 for no and 1 for yes: "))
+        if cont==0:
+            run_prog=False
+        else:
+            run_prog=True
+main()

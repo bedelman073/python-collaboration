69bd23a230c1ec6755bb6720ce9aa6ce3d8c461b
bedelman073 <edelman.b@northeastern.edu>
2024-02-21 12:28:12 -0500

    I created the program file

---
+++ todo_list.py
@@ -0,0 +1,10 @@
+def main():
+    print("""Welcome to the to do list editior
+    Please enter the number of the command you want
+    1.) Add a new task
+    2.) Mark a task as complete
+    3.) View your list
+    4.) Delete tasks""")
+    ans=input()
+    todo_list={}
+main()
